646648c6a78a18fac4e9fd240f710c085fcdc96c
Thennarasu <thennarasu4499@gmail.com>
2024-06-28 20:16:28 +0530

    [Thennarasu] Refactor code base

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import bodyParser from 'body-parser';
 import express from 'express';
-import pingRouter from './routes/ping.js';
 import signUpRouter from './routes/signUp.js';
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(pingRouter);
 app.use(signUpRouter);
 
 export default app;
