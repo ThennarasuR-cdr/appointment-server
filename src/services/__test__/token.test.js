aa9312680fe104c95922cf7c7dcbfb39215108a4
Thennarasu <thennarasu4499@gmail.com>
2024-06-28 20:21:25 +0530

    [Thennarasu] Add service to handle jwt tokens

---
+++ src/services/__test__/token.test.js
@@ -0,0 +1,78 @@
+import base64url from 'base64url';
+import { issueToken, verifyToken } from '../token.js';
+
+jest.mock('base64url');
+
+jest.mock('js-sha256', () => ({
+	sha256: { hmac: jest.fn().mockReturnValue('256') }
+}));
+
+const mockedBase64Url = base64url;
+
+describe('Token service', () => {
+	beforeEach(() => {
+		Date.now = jest.fn().mockReturnValue(10);
+	});
+
+	describe('Issue token', () => {
+		it('should return the JWT token', () => {
+			mockedBase64Url.encode.mockReturnValue('123');
+
+			const actualToken = issueToken({ email: 'validEmail' });
+
+			expect(actualToken).toBe('123.123.256');
+		});
+	});
+
+	describe('Verify token', () => {
+		const header = '{"alg": "HS256","typ": "JWT"}';
+		const payload = '{"email": "validEmail","issuedTime": "212112", "expirationTime": "0"}';
+
+		it('should return true for valid token', () => {
+			Date.now = jest.fn().mockReturnValue(-1);
+			mockedBase64Url.decode.mockReturnValueOnce(header);
+			mockedBase64Url.decode.mockReturnValueOnce(payload);
+
+			const actualResult = verifyToken('validHeader.validPayload.256');
+
+			expect(actualResult).toBe('validEmail');
+		});
+
+		it('should return false for expired token', () => {
+			Date.now = jest.fn().mockReturnValue(1);
+			mockedBase64Url.decode.mockReturnValueOnce(header);
+			mockedBase64Url.decode.mockReturnValueOnce(payload);
+
+			const actualResult = verifyToken('validHeader.validPayload.256');
+
+			expect(actualResult).toBeFalsy();
+		});
+
+		it('should return false for token with invalid header', () => {
+			mockedBase64Url.decode.mockReturnValueOnce('{"alg": "invalidAlg","typ": "JWT"}');
+			mockedBase64Url.decode.mockReturnValueOnce(payload);
+
+			const actualResult = verifyToken('validHeader.validPayload.256');
+
+			expect(actualResult).toBeFalsy();
+		});
+
+		it('should return false for token with invalid secret', () => {
+			mockedBase64Url.decode.mockReturnValueOnce(header);
+			mockedBase64Url.decode.mockReturnValueOnce(payload);
+
+			const actualResult = verifyToken('validHeader.validPayload.invalidSecret');
+
+			expect(actualResult).toBeFalsy();
+		});
+
+		it('should return false for token whose content are not JSON type', () => {
+			mockedBase64Url.decode.mockReturnValueOnce('not json');
+			mockedBase64Url.decode.mockReturnValueOnce(payload);
+
+			const actualResult = verifyToken('validHeader.validPayload.256');
+
+			expect(actualResult).toBeFalsy();
+		});
+	});
+}); 
---
+++ src/services/token.js
@@ -0,0 +1,55 @@
+import base64url from 'base64url';
+import { sha256 } from 'js-sha256';
+
+const tokenEncryptionSecret = process.env.TOKEN_ENCRYPTION_SECRET;
+
+export const issueToken = (payload) => {
+	const issuedTime = Date.now();
+	const expirationTime = Date.now() + (3600 * 1000);
+
+	const encodedHeader = base64url.encode(JSON.stringify({
+		alg: 'HS256',
+		typ: 'JWT'
+	}));
+
+	const encodedPayload = base64url.encode(JSON.stringify({ ...payload, issuedTime, expirationTime }));
+
+	const secret = sha256.hmac(tokenEncryptionSecret, `${encodedHeader}.${encodedPayload}`);
+
+	const token = `${encodedHeader}.${encodedPayload}.${secret}`;
+
+	return token;
+};
+
+export const verifyToken = (token) => {
+	const tokenItems = token.split('.');
+
+	let decodedHeader;
+	let decodedPayload;
+	try {
+		decodedHeader = JSON.parse(base64url.decode(tokenItems[0]));
+		decodedPayload = JSON.parse(base64url.decode(tokenItems[1]));
+	} catch (error) {
+		console.error('Error while parsing token items: ', error);
+		return false;
+	}
+
+	if (decodedHeader.alg !== 'HS256' || decodedHeader.typ !== 'JWT') {
+		console.log('Header is not matching');
+		return false;
+	}
+
+	const generatedSecret = sha256.hmac(tokenEncryptionSecret, `${tokenItems[0]}.${tokenItems[1]}`);
+
+	if (generatedSecret === tokenItems[2]) {
+		if (Date.now() > decodedPayload.expirationTime) {
+			console.log('Token expired');
+			return false;
+		}
+
+		return decodedPayload.email;
+	}
+
+	console.log('Secret is invalid');
+	return false;
+};
